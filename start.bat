3cd2f98debb0d452b49e971893c451cbd253f16c
remotehoster <87617668+remotehoster@users.noreply.github.com>
2021-07-18 20:39:26 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -16,7 +16,7 @@ echo  Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: androiddev
+echo Password: Vgy123$.
 echo ...
 echo  RDP By Android Developer
 echo   Please Login to Your RDP !
